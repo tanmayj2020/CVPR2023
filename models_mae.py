f5f342c0cf18a75b7dc7c6d560e4f77a68d06b36
Tanmay Jain <55263849+tanmayj2020@users.noreply.github.com>
2022-07-06 23:46:54 +0530

    Update models_mae.py

--- models_mae.py
+++ models_mae.py
@@ -287,10 +287,18 @@ def mae_vit_huge_dec512d8b(**kwargs):
 
 
 
+def mae_vit_small_dec128d2b(**kwargs):
+    model = MaskedAutoencoderViT(
+        embed_dim=384, depth=12, num_heads=6,
+        decoder_embed_dim=128, decoder_depth=2, decoder_num_heads=16,
+        mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs
+    )
+    return model
 
 
 # set recommended archs
 mae_vit_tiny = mae_vit_tiny_dec128d2b
+mae_vit_small = mae_vit_small_dec128d2b
 mae_vit_base = mae_vit_base_dec512d8b  # decoder: 512 dim, 8 blocks
 mae_vit_large = mae_vit_large_dec512d8b  # decoder: 512 dim, 8 blocks
 mae_vit_huge = mae_vit_huge_dec512d8b  # decoder: 512 dim, 8 blocks
